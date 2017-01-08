687debbdeeeeccf262d6f4076f1d40a367fe12dc
Huw Taylor <huw_taylor@hotmail.co.uk>
2017-01-08 13:02:51 +0000

    changed process to fixed_process to make it work, and fixed vector typo and lowered player speed

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,12 +1,12 @@
 extends KinematicBody2D
 
-export var player_speed = 200
+export var player_speed = 64
 
 func _ready():
 	set_fixed_process(true)
 
-func _process(delta):
-	var motion = Vector(0, 0)
+func _fixed_process(delta):
+	var motion = Vector2(0, 0)
 	if Input.is_action_pressed("move_up"):
 		motion.y -= 1
 	if Input.is_action_pressed("move_left"):
@@ -14,5 +14,5 @@ func _process(delta):
 	if Input.is_action_pressed("move_down"):
 		motion.y += 1
 	if Input.is_action_pressed("move_right"):
-		motion.x += 1	
+		motion.x += 1
 	move(motion.normalized() * player_speed * delta)
